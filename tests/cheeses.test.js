6ace075f35294f38608b72018e65aa5dcb1ddaf1
Meryl Turner <merylturner08@gmail.com>
2017-07-20 15:39:00 -0700

    gets all passes

--- tests/cheeses.test.js
+++ tests/cheeses.test.js
@@ -9,32 +9,39 @@ const url = 'mongodb://localhost:27017/cheeses-test';
 const app = require('../lib/app');
 
 describe('cheese resource', () => {
-    
+
     before(() => connection.connect(url));
     before(() => connection.db.dropDatabase());
-    
+
     const request = chai.request(app);
 
+    const cheese = [
+        { name: 'cheddar', origin: 'England' },
+        { name: 'brie', origin: 'France' },
+        { name: 'havarti', origin: 'Denmark' }
+    ];
+
     it('saves', () => {
-        const cheese = { name: 'cheddar', origin: 'England'};
         return request.post('/cheeses')
             .send(cheese)
             .then(res => {
                 const saved = res.body;
                 assert.ok(saved._id);
-                assert.equal(saved.name, cheese.name);
-                assert.equal(saved.origin, cheese.origin);
+                assert.equal(saved.name, cheese[0].name);
+                assert.equal(saved.origin, cheese[0].origin);
+            });
+
+    });
+
+    it('gets all', () => {
+        return request.get('/cheeses')
+            .then(res => {
+                const got = res.body;
+                assert.equal(got.length, cheese.length);
             });
     });
 
-    it('gets', () => {
-        return request.get('/cheeses')
-            .then(res => {
-                const got = res.body[0];
-                assert.ok(got._id);
-                assert.equal(got.name,'cheddar');
-                assert.equal(got.origin,'England');
-            });
+    it('gets all', () => {
 
     });
 });
